fbccf780293febd9e1584e5b06adb16d5ececf56
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-05-08 16:50:20 -0300

    Backend Feito

--- BackEnd/dataBase/conn.js
+++ BackEnd/dataBase/conn.js
@@ -3,7 +3,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'Fome'
+    database: 'fome'
 });
 
 connection.connect(err => {
