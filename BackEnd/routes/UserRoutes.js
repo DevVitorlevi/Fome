fb4c4cff0a077223ec712ed0d74fe31d73c13b39
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-05-10 09:24:23 -0300

    att

--- BackEnd/routes/UserRoutes.js
+++ BackEnd/routes/UserRoutes.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
-const { authMiddleware } = require('../controllers/UserController');
+
 
 
 router.post('/cadastro', UserController.register);
 router.post('/login', UserController.login);
-router.get('/home', authMiddleware, UserController.getHome); 
-router.get('/profile', authMiddleware, (req, res) => {
-    res.status(200).json({
-        mensagem: 'Bem-vindo ao seu perfil!',
-        usuario: req.user  
-    });
-});
+router.get('/home', UserController.getHome); 
 
 module.exports = router;
